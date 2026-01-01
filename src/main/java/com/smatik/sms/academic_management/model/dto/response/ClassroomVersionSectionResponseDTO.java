e28e1726c9ea7c2158e164a7186954552abd6bc4
afzalalhaia <afzalalhaia@gmail.com>
2026-01-01 10:59:02 +0600

    crect entity & dto

---
+++ src/main/java/com/smatik/sms/academic_management/model/dto/response/ClassroomVersionSectionResponseDTO.java
@@ -0,0 +1,18 @@
+package com.smatik.sms.academic_management.model.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClassroomVersionSectionResponseDTO {
+
+    private Long id;
+    private String className;
+    private String versionName;
+    private String sectionName;
+}
---
+++ src/main/java/com/smatik/sms/academic_management/model/dto/response/VersionResponseDTO.java
@@ -0,0 +1,16 @@
+package com.smatik.sms.academic_management.model.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class VersionResponseDTO {
+
+    private Long id;
+    private String name;
+}
