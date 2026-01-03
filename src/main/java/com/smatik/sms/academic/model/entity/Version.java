16dbea2a225636c119c75f8bdc3239400ac1c9cf
afzalalhaia <afzalalhaia@gmail.com>
2026-01-03 13:10:59 +0600

    Entitys

---
+++ src/main/java/com/smatik/sms/academic/model/entity/Version.java
@@ -0,0 +1,25 @@
+package com.smatik.sms.academic.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Version {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "version",cascade = CascadeType.ALL)
+    private List<ClassroomVersionSection> classroomVersionSections;
+}
