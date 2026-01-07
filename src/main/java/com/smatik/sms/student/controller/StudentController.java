c402ad193abf7d2efec87105e3bd6d6c397dd351
Abdur Rahman Aljami <aljamihaia24@gmail.com>
2026-01-07 15:08:28 +0600

    util class and student save method done

--- src/main/java/com/smatik/sms/student/controller/StudentController.java
+++ src/main/java/com/smatik/sms/student/controller/StudentController.java
@@ -1,5 +1,8 @@
 package com.smatik.sms.student.controller;
 
+import com.smatik.sms.academic.model.repository.ClassRoomRepository;
+import com.smatik.sms.academic.model.repository.SectionRepository;
+import com.smatik.sms.academic.model.repository.VersionRepository;
 import com.smatik.sms.common.address.dto.AddressRequestDto;
 import com.smatik.sms.common.address.entity.District;
 import com.smatik.sms.common.address.entity.Division;
@@ -12,7 +15,7 @@ import com.smatik.sms.common.enums.Gender;
 import com.smatik.sms.common.enums.IdentityType;
 import com.smatik.sms.student.model.dto.request.StudentRequestDto;
 import com.smatik.sms.student.service.StudentService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -28,16 +31,16 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/student")
+@RequiredArgsConstructor
 public class StudentController {
 
-    @Autowired
-    private DivisionRepository divisionRepository;
-    @Autowired
-    private DistrictRepository districtRepository;
-    @Autowired
-    private PoliceStationRepository policeStationRepository;
-    @Autowired
-    private StudentService studentService;
+    private final ClassRoomRepository classRoomRepository;
+    private final VersionRepository versionRepository;
+    private final SectionRepository sectionRepository;
+    private final DivisionRepository divisionRepository;
+    private final DistrictRepository districtRepository;
+    private final PoliceStationRepository policeStationRepository;
+    private final StudentService studentService;
 
     //  show form
 
@@ -50,6 +53,9 @@ public class StudentController {
         model.addAttribute("genders", Gender.values());
         model.addAttribute("identityTypes", IdentityType.values());
         model.addAttribute("title", "Student Form");
+        model.addAttribute("classRoom", classRoomRepository.findAll());
+        model.addAttribute("version", versionRepository.findAll());
+        model.addAttribute("section", sectionRepository.findAll());
         return "student/studentForm";
     }
 
