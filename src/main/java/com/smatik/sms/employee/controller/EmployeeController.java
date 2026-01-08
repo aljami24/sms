aa061fc37fec4d1d6a064950fe32e506d433c8da
afzalalhaia <afzalalhaia@gmail.com>
2026-01-08 11:08:04 +0600

    Afza scrum 173 test

--- src/main/java/com/smatik/sms/employee/controller/EmployeeController.java
+++ src/main/java/com/smatik/sms/employee/controller/EmployeeController.java
@@ -57,7 +57,7 @@ public class EmployeeController {
         }
 
         employeeService.saveEmployee(employeeFormDto);
-        return "redirect/employee/all";
+        return "redirect:/employee/employeeForm";
     }
 
 //    @GetMapping("/all")
