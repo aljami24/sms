b6929c7897d14c0cdc0902ce8a242225eab03e1d
Imran hossain <imranh8044@gmail.com>
2026-01-11 15:38:22 +0600

    Imran scrum 128 exception handler

---
+++ src/main/java/com/smatik/sms/common/exception/ResponseApi.java
@@ -0,0 +1,16 @@
+package com.smatik.sms.common.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseApi <T,W>{
+    private W message;
+    private T data;
+    private Integer code;
+    private String status;
+}
