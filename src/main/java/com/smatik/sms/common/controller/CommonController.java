f1a4b1bcc1de85469f28ebf379a3b310b23a3644
imran <imranh8044@gmail.com>
2026-01-12 11:35:54 +0600

    added Authentication & Authorization

---
+++ src/main/java/com/smatik/sms/common/controller/CommonController.java
@@ -0,0 +1,39 @@
+package com.smatik.sms.common.controller;
+
+import com.smatik.sms.employee.model.dto.response.EmployeeResponseDto;
+import com.smatik.sms.employee.service.EmployeeService;
+import com.smatik.sms.student.model.dto.response.StudentResponseDto;
+import com.smatik.sms.student.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class CommonController {
+    @Autowired
+    private StudentService studentService;
+    @Autowired
+    private EmployeeService employeeService;
+
+
+    @GetMapping
+    public String home(Model model){
+        List<StudentResponseDto> allStudent = studentService.getAllStudent();
+        Page<EmployeeResponseDto> allEmployee = employeeService.getAllEmployee(0, 10, "id", "desc");
+        model.addAttribute("allStudent",allStudent);
+        model.addAttribute("allEmployee",allEmployee);
+        return "dashboard";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+}
--- src/main/java/com/smatik/sms/user/model/entity/de.java
+++
@@ -1,4 +0,0 @@
-package com.smatik.sms.user.model.entity;
-
-public class de {
-}
